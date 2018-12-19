b24e7d0751e0d426073427205392bd72acb3b69d
Matthew Sigley <matthew.d.sigley@gmail.com>
2018-12-19 01:21:25 -0500

    Allow minor updates to be manually installed.

--- simple-wp-update-control/simple-wp-update-control.php
+++ simple-wp-update-control/simple-wp-update-control.php
@@ -3,16 +3,9 @@
 Plugin Name: Simple Wordpress Update Control
 Plugin URI: http://github.com/msigley/
 Description: Hides major core Wordpress, plugin, and theme update notifications for better version control. Also disables all auto-updates but minor/security Wordpress updates.
-Version: 1.0.3
+Version: 1.1.0
 Author: Matthew Sigley
 License: GPL2
-
-Change Log:
-- Added filter to prevent available major core updates from being saved in the db (1.0.3)
-- Updates page is visable again (1.0.3)
-- Updates page is now hidden in the admin menu (1.0.2)
-- Update notices are now hidden in the admin area (1.0.1)
-- Added auto-updater filters (1.0.0)
 */
 
 //Accidental update prevention
@@ -20,29 +13,41 @@ Change Log:
 function hide_admin_notices() {
     remove_action( 'admin_notices', 'update_nag', 3 );
 }
-add_action('admin_menu','hide_admin_notices');
+add_action( 'admin_menu','hide_admin_notices' );
 
 // Disable major core updates, but allow minor ones
-function simple_vc_disable_major_updates($updates) {
-		/* Test Cases
-		$test_update = clone $updates->updates[0];
-		$test_update->current = '3.8.2'; //Minor update
-		$updates->updates[] = $test_update;
-		$test_update = clone $updates->updates[0];
-		$test_update->current = '3.9.0'; //Major update
-		$updates->updates[] = $test_update;
-		*/
-		include ABSPATH . WPINC . '/version.php'; // $wp_version; // x.y.z
-		$current_branch = implode( '.', array_slice( preg_split( '/[.-]/', $wp_version  ), 0, 2 ) ); // x.y
-		foreach ( $updates->updates as $key => $update ) {
-			if(version_compare( $update->current, $wp_version, '>' )) {
-				$new_branch = implode( '.', array_slice( preg_split( '/[.-]/', $update->current ), 0, 2 ) ); // x.y
-				if(version_compare( $new_branch, $current_branch, '>' )) {
-					//Prevent major updates
-					unset($updates->updates[$key]);
-				}
-			}
-		}
+function simple_vc_disable_major_updates( $updates ) {
+    /* Test Cases
+    $test_update = clone $updates->updates[0];
+    $test_update->current = '3.8.2'; //Minor update
+    $updates->updates[] = $test_update;
+    $test_update = clone $updates->updates[0];
+    $test_update->current = '3.9.0'; //Major update
+    $updates->updates[] = $test_update;
+    */
+    if ( empty( $updates ) )
+        return $updates;
+
+    $minor_updates = array();
+    $last_minor_update = false;
+    include ABSPATH . WPINC . '/version.php'; // $wp_version; // x.y.z
+    $current_branch = substr( $wp_version, 0, strrpos( $wp_version, '.' ) ); // x.y
+    for ( $i = count( $updates->updates ) - 1; $i > -1; $i-- ) {
+        $update = $updates->updates[$i];
+        $new_branch = substr( $update->current, 0, strrpos( $update->current, '.' ) ); // x.y
+        if( version_compare( $new_branch, $current_branch, '=' ) ) { // Minor release
+            $minor_updates[] = clone $update;
+            $last_minor_update = clone $update;
+        }
+    }
+
+    if ( !empty( $last_minor_update ) && $last_minor_update->response == 'autoupdate' ) {
+        $last_minor_update->response = 'upgrade';
+        $minor_updates[] = $last_minor_update;
+    }
+
+    $updates->updates = array_reverse( $minor_updates );
+    
 	return $updates;
 }
 add_filter( 'pre_set_site_transient_update_core', 'simple_vc_disable_major_updates', 10, 1 );
